b87b3bad977fbba683160ab298837f5ff8eafd35
vibhcool <vibhcool@gmail.com>
2017-11-03 18:12:18 +0530

    comment refine urls

--- lib/refine_data.py
+++ lib/refine_data.py
@@ -6,11 +6,13 @@ from nltk.classify import NaiveBayesClassifier
 #TODO: Applies Naive Bayes Algo to check to get top 5 urls from feeded trained data and
 # check if url relevant using nlp, check if it is not social-media url
 def refine_urls(url_list, related_word):
+    return url_list
+'''
     demo_train = ['wikipedia','blog','blogspot','research','wiki']
     #train_sets=[({i.lower() for i in demo_train],'req'}
     req_urls= []
     
-    train_sets=[({'wikipedia':True},'req'),({'blog':True},'req'),({'blogspot':True},'req'),({'wiki':True,},'req'),({'facebook':False,'yahoo':False,'gnome':False},'notreq')]
+    train_sets = [({'wikipedia':True},'req'),({'blog':True},'req'),({'blogspot':True},'req'),({'wiki':True,},'req'),({'facebook':False,'yahoo':False,'gnome':False},'notreq')]
     print(train_sets)
     unsense={'facebook':False,'yahoo':False,'gnome':False}
     classifier = nltk.NaiveBayesClassifier.train(train_sets)
@@ -24,7 +26,7 @@ urls=['https://en.wikipedia.org/wiki/Akbar','www.culturalindia.net','www.faceboo
 r=[]
 refine_urls(urls,r)
 
-
+'''
 
 #TODO: Apply nlp and trained data to check it's relation from topics like tech, sports, etc
 def select_imp_data(data_list, related_word):
